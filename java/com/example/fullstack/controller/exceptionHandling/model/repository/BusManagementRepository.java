c1c1e0ad6a7ce6cd003397e331d62714f6812fc6
sangiiiii <119114411+sangiiiii@users.noreply.github.com>
2023-03-09 17:48:13 +0530

    Add files via upload
    fullstackproject

---
+++ java/com/example/fullstack/controller/exceptionHandling/model/repository/BusManagementRepository.java
@@ -0,0 +1,13 @@
+package com.example.fullstack.controller.exceptionHandling.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.fullstack.controller.exceptionHandling.model.BusManagement;
+
+
+
+public interface BusManagementRepository extends JpaRepository<BusManagement, Integer>{
+
+
+
+}
+
